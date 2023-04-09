815b519c2357928008ec108ea2c6d39701a1bf46
kurosiroke <ec2-user@ip-172-31-34-239.ap-northeast-1.compute.internal>
2023-04-09 22:46:30 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'lists#create'
+
+
   get '/top' => 'homes#top'
-  get 'books/show'
-  get 'books/edit'
-  get 'books'=>'books#index'
+
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
